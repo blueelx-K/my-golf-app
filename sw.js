8e57db3e432b1391c19a5647bda1cc0c0e35b7c8
blueelx-K <blueelx@gmail.com>
2026-09-22 21:39:36 +0900

    Add per-hole memo with quick tags and round review screen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'golf-scorecard-v1';
+const CACHE_NAME = 'golf-scorecard-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (event) {
